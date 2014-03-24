21ca543b97803c641a24a0bec1d1c8738980a61f
Yanzhi <kudutest@outlook.com>
2014-03-24 16:21:58 +0800

    0324

--- MvcApplication1/Controllers/HomeController.cs
+++ MvcApplication1/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace MvcApplication1.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "My awesome ASP.NET MVC application with changes 0321.";
+            ViewBag.Message = "My awesome ASP.NET MVC application with changes 0324.";
 
             return View();
         }
